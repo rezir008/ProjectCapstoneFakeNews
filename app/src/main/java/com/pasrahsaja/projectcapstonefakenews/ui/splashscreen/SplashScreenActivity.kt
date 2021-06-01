13128780828725119eff422308a824f43ef3593e
rezir008 <rezir008@gmail.com>
2021-06-01 16:50:12 +0800

    rapiin beberapa code

--- app/src/main/java/com/pasrahsaja/projectcapstonefakenews/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/pasrahsaja/projectcapstonefakenews/ui/splashscreen/SplashScreenActivity.kt
@@ -5,12 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ImageView
 import android.widget.ProgressBar
 import com.pasrahsaja.projectcapstonefakenews.R
-import com.pasrahsaja.projectcapstonefakenews.databinding.ActivitySplashScreenBinding
 import com.pasrahsaja.projectcapstonefakenews.ui.home.HomeActivity
 
+@Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
     private val TIME: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
